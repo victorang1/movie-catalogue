2326add344f1ce38de77fa917e222149a92a85fa
Victor <angvictor91@gmail.com>
2020-10-21 15:16:44 +0700

    adding consntat film type

---
+++ app/src/main/java/com/example/moviecatalogue/constant/FilmType.kt
@@ -0,0 +1,13 @@
+package com.example.moviecatalogue.constant
+
+import android.content.res.Resources
+import com.example.moviecatalogue.R
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+
+object FilmType : KoinComponent {
+
+    val resources: Resources by inject()
+    var MOVIE = resources.getString(R.string.text_type_movie)
+    var TV_SHOW = resources.getString(R.string.text_type_tv_show)
+}
